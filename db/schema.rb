3c109dad58c9da778c45da23f1b59f6764a42c6b
Evan Shabsove <evan.shabsove@gmail.com>
2017-01-04 09:34:05 -0500

    added categories table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104141802) do
+ActiveRecord::Schema.define(version: 20170104141956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "code"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "inventory_products", force: :cascade do |t|
     t.string   "inventory_code"
