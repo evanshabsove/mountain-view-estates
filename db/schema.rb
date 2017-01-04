6f18eaa19cc52bbd2f3cdcef0634c300de6d40fe
Evan Shabsove <evan.shabsove@gmail.com>
2017-01-04 10:13:07 -0500

    made Order model as well as added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104150944) do
+ActiveRecord::Schema.define(version: 20170104151234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,11 @@ ActiveRecord::Schema.define(version: 20170104150944) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer  "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
