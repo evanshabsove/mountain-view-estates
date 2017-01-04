aaf85bfa0bfdd0f006b5823237fa80992b4b07e4
Evan Shabsove <evan.shabsove@gmail.com>
2017-01-04 10:15:33 -0500

    added order status model and pushed it into the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104151234) do
+ActiveRecord::Schema.define(version: 20170104151352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,12 @@ ActiveRecord::Schema.define(version: 20170104151234) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer  "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_statuses", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
