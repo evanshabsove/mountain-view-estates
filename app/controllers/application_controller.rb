46664ecc13cd9120f26403ce9b842bc1dc45ba66
Evan Shabsove <evan.shabsove@gmail.com>
2017-01-06 13:40:34 -0500

    can't get button to display shoping cart working, will try and fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_order
+
   def after_sign_in_path_for(resource_or_scope)
     if current_user.admin
       return admin_index_url
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ApplicationRecord
   has_many :order_items
   belongs_to :user
+  belongs_to :order_status
+  before_create :set_order_status
+
+  private
+  def set_order_status
+    self.order_status_id = 1
+  end
+
+
 end
