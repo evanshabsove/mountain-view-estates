a508cac517642e30454c55e11a5f77cd323414fe
Evan Shabsove <evan.shabsove@gmail.com>
2017-01-06 10:35:42 -0500

    created admin user, when you log in website checks if you are admin. Will create only admin can create new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def after_sign_in_path_for(resource_or_scope)
+    if @user.admin
+      redirect_to admin_home
+    else
+      redirect_to order_home
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    redirect_to user_session
+  end
+
+  def authorize_admin
+    return unless !current_user.admin?
+    redirect_to root_path, alert: 'Admins only!'
+  end
 end
