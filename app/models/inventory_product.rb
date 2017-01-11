c6b7447bbdf1403d014e044370f264a7b9e01735
Evan Shabsove <evan.shabsove@gmail.com>
2017-01-11 15:58:04 -0500

    added defined search for coffee products

--- app/models/inventory_product.rb
+++ app/models/inventory_product.rb
@@ -1,10 +1,11 @@
 class InventoryProduct < ApplicationRecord
   has_many :order_items
 
-  def self.search(search, product_code)
+  def self.search(search, product_code, def_search)
     if search
       up = search.upcase
-      where('description LIKE ? AND product_code = ?', "%#{up}%", product_code)
+
+      where('description LIKE ? AND product_code = ? AND description LIKE ?', "%#{up}%", product_code, "%#{def_search}%")
     else
       InventoryProduct.all
     end
