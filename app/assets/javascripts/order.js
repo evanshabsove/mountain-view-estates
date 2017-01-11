1a497a6b6f134f0e0d10df6b0869df7727350ce7
Evan Shabsove <evan.shabsove@gmail.com>
2017-01-11 17:01:16 -0500

    changed to checkboxes, added jquery to make them act like radio buttons. sub catagories uncheck when click on other catagory

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,7 +1,35 @@
 
-// $(function() {
-//   $("#products_search input").keyup(function(){
-//     $.get($("#products_search").attr("action"), $("#products_search").serialize(), null, "script");
-//     return false;
-//   });
-// });
+$(function() {
+  // $("#products_search input").keyup(function(){
+  //   $.get($("#products_search").attr("action"), $("#products_search").serialize(), null, "script");
+  //   return false;
+  // });
+  $(".coffee-button").on("click",function(){
+    $("#coffee-hidden").css({
+      display: 'block'
+    });
+  });
+
+  $(".other-button").on("click",function(){
+    $("#coffee-hidden").css({
+      display: 'none'
+    });
+    $(".hidden-button").prop('checked',false)
+  });
+
+  $(".checkbox").change(function() {
+      var checked = $(this).is(':checked');
+      $(".checkbox").prop('checked',false);
+      if(checked) {
+          $(this).prop('checked',true);
+      }
+  });
+
+  $(".hidden-button").change(function() {
+      var checked = $(this).is(':checked');
+      $(".hidden-button").prop('checked',false);
+      if(checked) {
+          $(this).prop('checked',true);
+      }
+  });
+});
