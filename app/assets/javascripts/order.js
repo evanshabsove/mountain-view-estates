9edc66c3137c96be850fe03351e083c548bb9c4b
Evan Shabsove <evan.shabsove@gmail.com>
2017-01-10 19:38:32 -0500

    commented out ajax, not sure if I want it or not

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,7 +1,7 @@
 
-$(function() {
-  $("#products_search input").keyup(function(){
-    $.get($("#products_search").attr("action"), $("#products_search").serialize(), null, "script");
-    return false;
-  });
-});
+// $(function() {
+//   $("#products_search input").keyup(function(){
+//     $.get($("#products_search").attr("action"), $("#products_search").serialize(), null, "script");
+//     return false;
+//   });
+// });
--- app/models/inventory_product.rb
+++ app/models/inventory_product.rb
@@ -1,10 +1,10 @@
 class InventoryProduct < ApplicationRecord
   has_many :order_items
 
-  def self.search(search)
+  def self.search(search, inventory_code)
     if search
       up = search.upcase
-      where('description LIKE ?', "%#{up}%")
+      where('description LIKE ? AND inventory_code LIKE ?', "%#{up}%", inventory_code)
     else
       InventoryProduct.all
     end
