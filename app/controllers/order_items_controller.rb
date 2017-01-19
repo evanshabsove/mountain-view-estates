73d25c1808196e9b3da9f8f572de1a722360cffb
Evan Shabsove <evan.shabsove@gmail.com>
2017-01-19 15:10:30 -0500

    changed ajax request a bit

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -18,8 +18,17 @@ class OrderItemsController < ApplicationController
   def update
     @order = current_order
     @order_item = @order.order_items.find(params[:id])
-    @order_item.update_attributes(order_item_params)
     @order_items = @order.order_items
+    respond_to do |format|
+      if @order_item.update_attributes(order_item_params)
+        format.html { redirect_to order_index_url, notice: 'Item was successfully updated.' }
+        format.js   { }
+        format.json { render order_index_url, status: :created, location: @order_item }
+      else
+        format.html { render order_index_url }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
