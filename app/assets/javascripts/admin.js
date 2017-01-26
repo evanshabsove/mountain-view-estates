b0876c94cd48c82ff0c89c4729559644296718a2
Evan Shabsove <evan.shabsove@gmail.com>
2017-01-26 11:45:17 -0500

    removed ajax request from user search, I think I am going to re-do how I add special products to users. Make it more stream line

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,8 +1,8 @@
 $(function(){
-  $("#users_search input").keyup(function() {
-    $.get($("#users_search").attr("action"), $("#users_search").serialize(), null, "script");
-    return false;
-  });
+  // $("#users_search input").keyup(function() {
+  //   $.get($("#users_search").attr("action"), $("#users_search").serialize(), null, "script");
+  //   return false;
+  // });
 
   $("#special_search input").keyup(function() {
     $.get($("#special_search").attr("action"), $("#special_search").serialize(), null, "script");
